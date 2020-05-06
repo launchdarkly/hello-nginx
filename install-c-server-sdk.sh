18999b8b8dd5e76d583e2a5f54ce179efdbbb56a
Harpo Roeder <hroeder@launchdarkly.com>
2020-05-06 13:29:20 -0700

    update install script to build shared library and change artifact name

--- install-c-server-sdk.sh
+++ install-c-server-sdk.sh
@@ -9,10 +9,10 @@ git clone https://github.com/launchdarkly/c-server-sdk.git
 cd c-server-sdk
 mkdir build && cd build
 
-cmake ..
-make
+cmake -D BUILD_TESTING=OFF -D BUILD_SHARED_LIBS=ON ..
+cmake --build .
 
-cp libldserverapidynamic.so /usr/lib/libldserverapi.so
+cp libldserverapi.so /usr/lib/libldserverapi.so
 chmod a+rwx /usr/lib/libldserverapi.so
 
 rm -rf $TMP_DIR
